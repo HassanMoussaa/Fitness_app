aa31b2714f5e785f6b1bb2c87a8584c2c1c53f0c
Hassan Moussa <hassanmoussa61@gmail.com>
2023-11-01 20:45:29 +0200

    Added Hero Banner

---
+++ src/components/Exercises.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Exercises() {
+  return <div></div>;
+}
+
+export default Exercises;
---
+++ src/components/SearchExercises.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchExercises() {
+  return <div></div>;
+}
+
+export default SearchExercises;
